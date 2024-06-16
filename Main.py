7b2f017d6489f4f4cce14be84f47ad1231a5f381
oc-Kaelan-Moodley-2024 <159388692+oc-Kaelan-Moodley-2024@users.noreply.github.com>
2024-06-16 03:26:22 +0000

    Jun 16, 2024, 3:26 AM

---
+++ Main.py
@@ -0,0 +1,49 @@
+"""
+Main.
+
+Created by: Kaelan Moodley
+Date: 28/05/2024
+
+Enviromental Conservation Game.
+
+"""
+
+# Enter your code here
+
+import random
+from typing import List, Tuple
+
+class Hero:
+    def __init__(self, name: str, health: int, attack_power: int):
+        """
+        Initialize a Character object.
+
+        Parameters:
+        - The name of the character.
+        - The health points of the character.
+        - The attack power of the character.
+        """
+        self.name = name
+        self.health = health
+        self.attack_power = attack_power
+
+        
+
+class Enemy(Hero):
+    def _init_(self, name: str, health: int, attack_power: int, reward: int):
+        """
+        Initialize an Enemy object.
+
+        Parameters: 
+
+        -The name of the enemy.
+        -The health points of the enemy.
+        -The attack power of the enemy.
+        -The currency reward for defeating the enemy.
+
+        """
+        super().__init__(name, health, attack_power)
+        self.reward = reward
+        
+    def main():
+        """ The main function to run the game. """
