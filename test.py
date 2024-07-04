22f91a09b62929feff822e937eeed5e9af6b540d
oc-Kaelan-Moodley-2024 <159388692+oc-Kaelan-Moodley-2024@users.noreply.github.com>
2024-07-04 20:39:58 +0000

    Jul 4, 2024, 8:39 PM

--- test.py
+++
@@ -1,7 +0,0 @@
-dic = {"bobby": 100}
-
-def function(monster; enemy_name) -> None:
-    input = input("enter number")
-    if input == "1":
-        function("bobby")
-    dic[monster] -= attack[damage]
