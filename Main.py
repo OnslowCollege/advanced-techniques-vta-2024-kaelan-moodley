323e8ab5d2fd2c30dd625c214493619fe6c619a6
oc-Kaelan-Moodley-2024 <159388692+oc-Kaelan-Moodley-2024@users.noreply.github.com>
2024-07-26 02:44:21 +0000

    Jul 26, 2024, 2:44 AM

--- Main.py
+++ Main.py
@@ -463,7 +463,7 @@ def main():
                 enemy_choice = int(enemy_choice) - 1
                 if 0 <= enemy_choice < len(enemies):
                     if not battle(player, enemies[enemy_choice]):
-                        print("Game over! {enemy_choice} has won ")
+                        print("Game over!")
                         break
                     enemies.pop(enemy_choice)
                 else:
