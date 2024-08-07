998ce8b21feda3211830641786f4762269d3ad06
oc-Kaelan-Moodley-2024 <159388692+oc-Kaelan-Moodley-2024@users.noreply.github.com>
2024-08-07 23:52:23 +0000

    Aug 7, 2024, 11:52 PM

--- Main.py
+++ Main.py
@@ -422,11 +422,11 @@ def main():
 
     player = Player(player_name)
     enemies_data = [
-        ("Deforestation", 110, 12, 100 ),
-        ("Pollution", 120, 14, 35 ),
-        ("Climate Change", 130, 15, 40 ),
-        ("Overfishing", 140, 20, 45 ),
-        ("Plastic Pollution", 150, 25, 75 ),
+        ("Deforestation", 110, 12, 100),
+        ("Pollution", 120, 14, 35),
+        ("Climate Change", 130, 15, 40),
+        ("Overfishing", 140, 20, 45),
+        ("Plastic Pollution", 150, 25, 75),
     ]
 
     enemies = [
@@ -482,4 +482,3 @@ def main():
 if __name__ == "__main__":
 
     main()
-
